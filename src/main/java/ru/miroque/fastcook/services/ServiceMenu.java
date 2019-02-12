5210e1a545a60460b705cbc7e93dadfa324d6777
Panov Aleksander <a.panov@24profi.ru>
2019-02-12 15:51:52 +0700

    ну пробудем дальше по PDD

--- src/main/java/ru/miroque/fastcook/services/ServiceMenu.java
+++ src/main/java/ru/miroque/fastcook/services/ServiceMenu.java
@@ -1,9 +1,24 @@
 package ru.miroque.fastcook.services;
 
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 
+import lombok.Getter;
+import ru.miroque.fastcook.domains.Dish;
 
- // @todo #1 Вывод меню для заказа 
+// @todo #1 Вывод меню для заказа 
+@ApplicationScoped
+@Named
 public class ServiceMenu {
-
+	@Getter
+	private List<Dish> menu;
+	
+	@PostConstruct
+	private void init() {
+		//@todo #1 сервис инициализации списка блюд
+		// в отдельный класс сервис
+	}
 }
